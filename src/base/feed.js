c0fab987e30ce2eeca036e64e1a73c1a1700b500
Your Name <you@example.com>
2019-01-22 18:36:56 +0100

    real initial commit

---
+++ src/base/feed.js
@@ -0,0 +1,55 @@
+var SocialFeed = function(selector, params) {
+    var instance = {},
+        apiRequest = new XMLHttpRequest();
+        requestUrl = 'https://api.instagram.com/v1/users/self/media/recent/?access_token=';
+        currentUrl = '',
+        _self = this,
+
+    this.defaults = {
+        count: 5,
+        loadMax: 5,
+        template: this.template,
+        requestSuccess: function(){},
+        onComplete: function(){},
+        onCompleteAll: function(){},
+        onLoadMoreComplete: function(){}
+    };
+
+    this.loadCount = 0;
+    this.counter = 0;    
+    this.container = (typeof selector == "string") ? document.querySelectorAll(selector) : selector;
+    
+    if(typeof this.container == "object" && this.container.length > 1) {
+        console.error('There is more than one Element matching the Selector "' + selector + '"');
+        return false;
+    } else if (this.container.length != "undefined" && !J.isNodeElement(this.container)) {
+        this.container = this.container[0];
+    }
+
+    this.params = J.merge(this.defaults, params);
+    
+    this.apiRequestLoaded = function() {
+        if(this.status === 200) {
+            _self.response = JSON.parse(this.response);
+            _self.params.requestSuccess(_self);
+            _self.container.innerHTML += _self.createMarkup();
+            _self.loadCount++;
+        } else {
+            console.error('Request Failed:' , this.responseText);
+        }
+    }
+    this.callRequest = function(url, maxId) {
+        apiRequest.addEventListener('load', this.apiRequestLoaded);
+        currentUrl = (typeof maxId !== "undefined") ? this.buildRequestUrl(url, maxId) : this.buildRequestUrl(url);
+        apiRequest.open('GET', currentUrl);
+        apiRequest.send();
+    }
+    this.init = function() {
+        _self.callRequest(requestUrl);
+        return this;
+    }
+    this.loadMore = function() {
+        if(_self.loadCount < _self.params.loadMax) _self.callRequest(requestUrl, _self.response.pagination.next_max_id);
+    }
+}
+
